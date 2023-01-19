38a14beb66bf9ef9b930a0af0cdc4f3ed9bdf1a8
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2023-01-19 11:30:09 +0530

    Create DetectCycle.java

---
+++ LinkedList/DetectCycle.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class DetectCycleLL{
+	public static class Node{
+		int data;
+		Node next;
+		public Node(int data){
+			this.data=data;
+			this.next = null;
+		}
+	}
+	public static Node head;
+	public static Node tail;
+	public static int size;
+	
+	public static boolean isCycle(){ //floyd cycle detect Algo
+		Node slow=head;
+		Node fast = head;
+		while(fast!=null && fast.next!=null){
+			slow=slow.next ;//+1
+			fast = fast .next.next ; //+2
+			if(slow==fast)
+				return true;
+		}
+		return false;
+	}
+	public static void main(String[] args){
+		head = new Node(1);
+		head.next=new Node(2);
+		head.next.next = new Node(3);
+		//1->2->3
+		// head.next.next.next = head;
+		//1->2->3->1
+		System.out.println(isCycle());
+	}
+}
